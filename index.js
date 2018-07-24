51b10cfe7a4f658777415e6e25b2d05cc748101f
Anthony Ashurst <anthoasho@gmail.com>
2018-07-25 02:17:22 +0900

    port alteration

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ app.use("/api/messages/:mid/like", helpers.likeMessage);
 app.get("/api/messages/", helpers.getGetAllMessages);
 app.get("/api/message/:mid/likes", helpers.getMessageLikes);
 app.get("/api/discover/users", helpers.getDiscoverUsers);
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
+console.log(PORT)
 app.listen(PORT, function(){
   console.log(`Server is listening on port ${PORT}`);
 });
